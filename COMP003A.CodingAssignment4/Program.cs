1d7bcefe74763aa3615cfda92539cc3d7b696616
Hasturious <jbster559@gmail.com>
2025-02-05 18:28:53 -0800

    Added menu method

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -12,8 +12,39 @@ namespace COMP003A.CodingAssignment4
             int[] inventory = new int[10];
 
 
+            Menu();
 
-            Console.Write("1. Add a Product \n2. Update Product Summary \n3. View Inventory Summary \n4. Exit\nEnter Option:");
+
+
+
+
+            static void Menu()
+            {
+                Console.Write("1. Add a Product \n2. Update Product Summary \n3. View Inventory Summary \n4. Exit\nEnter Option:");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+            
+                }
+                else if (choice == 2)
+                {
+
+                }
+                else if (choice == 3)
+                {
+
+                }
+                else if (choice == 4)
+                {
+
+                }
+                else
+                {
+                Console.WriteLine("Invalid Input");
+                Menu();
+                }
+            }
         }
     }
 }
