a315ed76675f2e52fc72caa78403df0d71df9550
Hasturious <jbster559@gmail.com>
2025-02-05 20:00:50 -0800

    Added try catch, main menu and product list

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -3,29 +3,36 @@
 // Faculty: Jonathan Cruz
 // Purpose: Inventory management application with a minimum of 10 elements in the collection.
 
+using System.Linq.Expressions;
+
 namespace COMP003A.CodingAssignment4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] inventory = new int[10];
+            List<string> productInventory = new List<string>();
+            int choice = 0;
 
-
-            Menu();
-
-
-
-
-
-            static void Menu()
+            while (choice != 4)
             {
-                Console.Write("1. Add a Product \n2. Update Product Summary \n3. View Inventory Summary \n4. Exit\nEnter Option:");
-                int choice = int.Parse(Console.ReadLine());
+                Console.Write("1. Add a Product \n2. Update Product Summary \n3. View Inventory Summary \n4. Exit\nEnter Option: ");
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
 
                 if (choice == 1)
                 {
-            
+                    Console.Write("Enter Product Name: ");
+                    string newProduct = Console.ReadLine();
+                    Console.Write("Enter Product Quantity: ");
+                    int wawa = int.Parse(Console.ReadLine());
+                    productInventory.Add(newProduct);
                 }
                 else if (choice == 2)
                 {
@@ -33,17 +40,22 @@ namespace COMP003A.CodingAssignment4
                 }
                 else if (choice == 3)
                 {
-
+                    foreach (string product in productInventory)
+                    {
+                        Console.WriteLine(product);
+                    }
                 }
                 else if (choice == 4)
                 {
-
+                    Console.WriteLine("Goodbye!");
+                    Console.ReadKey(); //On any keystroke the terminal closes
+                    Environment.Exit(0);
                 }
                 else
                 {
-                Console.WriteLine("Invalid Input");
-                Menu();
+                    Console.WriteLine("Invalid Input");
                 }
+
             }
         }
     }
