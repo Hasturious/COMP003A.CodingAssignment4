869669e2dc43e0a75194229db7df615642d29881
Hasturious <jbster559@gmail.com>
2025-02-05 18:17:42 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Author: Jean Bryant Figueroa
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Inventory management application with a minimum of 10 elements in the collection.
+
+namespace COMP003A.CodingAssignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inventory = new int[10];
+
+
+
+            Console.Write("1. Add a Product \n2. Update Product Summary \n3. View Inventory Summary \n4. Exit\nEnter Option:");
+        }
+    }
+}
